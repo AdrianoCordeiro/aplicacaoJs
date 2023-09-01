63712a11fc301fdcef1e392c59bf419e8637d9e6
AdrianoCordeiro <adr.sud.cor@gmail.com>
2023-08-31 22:03:31 -0300

    atualizacao310823

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const convertButton = document.querySelector(".convert-button")
 const currencySelect = document.querySelector(".currency-select")
 
 function convertValues() {
+
     const inputCurrencyValue = document.querySelector(".input-currency").value /*Criação de variavél*/
     const currencyValueToConvert = document.querySelector(".currency-value-to-convert") //valor em real
     const currencyValueConverted = document.querySelector(".currency-value") //outras Moedas
@@ -13,13 +14,14 @@ function convertValues() {
     const euroToday = 6.2
 
 
-    if (currencySelect.value == "dolar") {
+    if(currencySelect.value == "dolar") {
         currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
             style: "currency",
             currency: "USD"
         }).format(inputCurrencyValue / dolarToday)
     }
-    if (currencySelect.value == "euro") {
+
+    if(currencySelect.value == "euro") {
         currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
             style: "currency",
             currency: "EUR"
@@ -28,9 +30,28 @@ function convertValues() {
 
     currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-br", {
         style: "currency",
-        currency: "BRL"
+        currency: "BRL",
     }).format(inputCurrencyValue)
+
 
 }
 
+function changeCurrency() {
+
+const currencyName = document.getElementById("currency-name")
+const currencyImage = document.querySelector(".currency-img")
+
+    if (currencySelect.value == "dolar"){
+        currencyName.innerHTML = "Dolar Americano"
+        currencyImage.src = "./assets/Dollar.png/"
+    }
+
+    if (currencySelect.value == "euro"){
+        currencyName.innerHTML = "Euro"
+        currencyImage.src = "./assets/Euro.png"
+    }
+
+}
+
+currencySelect.addEventListener("change", changeCurrency)
 convertButton.addEventListener("click", convertValues) /* ouvinte de Eventos*/
